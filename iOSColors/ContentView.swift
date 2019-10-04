b8d7e3576e7d0e8e844d7ede1f662f844554c0eb
Justin Allen <all12jus@gmail.com>
2019-10-04 06:25:43 -0400

    Initial Commit

---
+++ iOSColors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSColors
+//
+//  Created by Justin Allen on 10/4/19.
+//  Copyright © 2019 Justin Allen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
